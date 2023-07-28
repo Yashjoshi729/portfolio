32c70564c3b0e36d6797d28c73d16d1a2d13d8f1
Yash <yashjoshi729@gmail.com>
2023-07-28 11:30:20 +0530

    new add

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-document.querySelector('.cross').style.display="none" 
-document.querySelector('.hamburger').addEventListener('click',()=>{
-    document.querySelector('.sidebar').classList.toggle('sidebargo');
-    if(document.querySelector('.sidebar').classList.contains('sidebargo')){
-        document.querySelector('.ham').style.display="inline"
-        document.querySelector('.cross').style.display="none"
+let ham=document.querySelector('.ham')
+let box=document.querySelector('.box')
+let cross=document.querySelector('.cross')
+let side=document.querySelector('.sidebar')
+let tweet=document.querySelector('.home-sci .tweet')
+
+cross.style.display="none";
+
+ham.addEventListener('click',()=>{
+    
+    side.classList.toggle("sidebargo")
+    if(side.classList.contains('sidebargo')){
+        box.style.display="inline";
+        cross.style.display="none";
     }
     else{
-        document.querySelector('.ham').style.display="none"
-        setTimeout(() => {
-            document.querySelector('.cross').style.display="inline"
-        }, 300);
+        box.style.display="none";
+        cross.style.display="inline";
     }
-}) 
+})  
+    let typed = new Typed('.text',{
+    strings:["Programmer","Front-End Developer","Footballer"], 
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+    })
